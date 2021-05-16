bf1d707641be8ecc6a7fba6052a80c8469a7fd38
Lee Hyungi <hyungi.lee.622@gmail.com>
2021-05-17 05:33:10 +0900

    feat: Next.js에 react-saga 적용
    - login/logout 비동기 처리에 대한 제너레이터 함수 작성(sagas/index.js)
    - 기존의 store에 redux-saga middleware 추가
    - App 컴포넌트를 next-redux-saga의 withReduxSaga HOC로 감싸서 처리

--- front/store/configureStore.js
+++ front/store/configureStore.js
@@ -2,16 +2,24 @@ import { createWrapper } from 'next-redux-wrapper';
 import { applyMiddleware, compose, createStore } from 'redux';
 import reducer from '../reducers';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import createSagaMiddleware from 'redux-saga'
+
+const loggerMiddleware = () => (next) => (action) => {
+    console.log(action);
+    return next(action);
+};
 
 const configureStore = () => {
+    const sagaMiddleware = createSagaMiddleware();
     // 개발용일때에만 devTools를 붙인다. (보안적 요소 고려)
     // history가 쌓이게 되면 메모리도 많이 잡아먹는다.
-    const middlewares = [];
+    const middlewares = [sagaMiddleware, loggerMiddleware];
     const enhancer = process.env.NODE_ENV === 'production' ?
         // 개발할때에는 redux-saga, thunk만 추가해준다.
         compose(applyMiddleware(...middlewares)) :
         composeWithDevTools(applyMiddleware())
     const store = createStore(reducer, enhancer);
+    store.sagaTask = sagaMiddleware.run(rootSaga);
     return store;
 }
 
