ad4ed0b855bbdac7ed4957e0af1fb1cb4aebe68c
Lee Hyungi <hyungi.lee.622@gmail.com>
2021-05-25 13:58:26 +0900

    feat: Nickname 업데이트 요청에 대한 상수 텍스트 추가

--- front/sagas/user.js
+++ front/sagas/user.js
@@ -1,6 +1,6 @@
 import { all, fork, call, put, delay, takeLatest } from 'redux-saga/effects';
 import axios from 'axios';
-import { LOG_IN_FAILURE, LOG_IN_SUCCESS, LOG_OUT_FAILURE, LOG_OUT_SUCCESS } from '../types/user';
+import { LOG_IN_FAILURE, LOG_IN_REQUEST, LOG_IN_SUCCESS, LOG_OUT_FAILURE, LOG_OUT_REQUEST, LOG_OUT_SUCCESS, SIGN_UP_FAILURE, SIGN_UP_REQUEST, SIGN_UP_SUCCESS } from '../types/user';
 
 // generator(*)
 // 패턴대로 코딩을 하기 때문에 익숙해지면 된다.
